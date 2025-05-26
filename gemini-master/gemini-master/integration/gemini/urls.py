f4ff06220529a752d2dbc9459bd5e7946bfb1a99
Vitor Waltrick <151741447+w4ltrick@users.noreply.github.com>
2025-05-26 16:17:51 -0300

    Delete gemini-master directory

--- gemini-master/gemini-master/integration/gemini/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('send_message', views.send_message, name='send_message'),
-]
--- gemini-master/integration/integration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('gemini/', include('gemini.urls')),
-    path('', RedirectView.as_view(url='gemini/', permanent=False)),  # Redirect root to gemini/
-]
